64257ab31be35e8d1cbe99cc91fcb944ac41561f
TovenaarAndreas <andreas.veeckman@tovenaargames.be>
2024-10-10 11:12:43 +0200

    Dlg type tags

--- Source/DlgSystem/DlgConstants.cpp
+++ Source/DlgSystem/DlgConstants.cpp
@@ -4,6 +4,9 @@
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_Dlg,			"Dlg");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Dlg_Cat,		"Dlg.Cat");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Dlg_Critter,	"Dlg.Critter");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Dlg_Frog,	"Dlg.Frog");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Dlg_Hero,	"Dlg.Hero");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Dlg_Human,	"Dlg.Human");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Dlg_Object,	"Dlg.Object");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Dlg_Other,	"Dlg.Other");
